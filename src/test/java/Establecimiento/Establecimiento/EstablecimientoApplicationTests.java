d838070f12de7b6f5ffa53f9da6ea75e73a04660
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-15 11:12:09 -0400

    inicio

---
+++ src/test/java/Establecimiento/Establecimiento/EstablecimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package Establecimiento.Establecimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstablecimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
