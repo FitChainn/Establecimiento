10fa8ade7e65067c19acca2d663dcf578b08c190
neurott <neurott@protonmail.com>
2026-06-08 10:37:36 -0400

    openapicfg

---
+++ src/main/java/Establecimiento/Establecimiento/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package Establecimiento.Establecimiento.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Establecimiento API - FitChain")
+                        .description("API GESTIÓN DEL ESTABLECIMIENTO")
+                        .version("1.0.0")).servers(List.of(
+                        new Server().url("http://localhost:8084").description("Local")
+                ));
+    }
+}
