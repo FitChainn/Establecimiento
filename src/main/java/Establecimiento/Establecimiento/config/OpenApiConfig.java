f45a934e7befc62f3da35753562ef0ae1ca2867f
neurott <neurott@protonmail.com>
2026-06-17 16:12:45 -0400

    .

--- src/main/java/Establecimiento/Establecimiento/config/OpenApiConfig.java
+++ src/main/java/Establecimiento/Establecimiento/config/OpenApiConfig.java
@@ -1,4 +1,17 @@
 package Establecimiento.Establecimiento.config;
 
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Entrenador API - FitChain")
+                        .description("API DE GESTIÓN DEL ENTRENADOR")
+                        .version("1.0.0")).servers(List.of(
+                        new Server().url("http://44.197.145.9:8082").description("AWS"),
+                        new Server().url("http://localhost:8082").description("Local")
+                ));
+    }
 }
